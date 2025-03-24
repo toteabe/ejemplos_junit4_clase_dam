b8c4cf1106a9d33bf0165d6bf6bcdc58b09b8727
jmmartin <jmmartin@1-dam-prof>
2025-03-24 13:58:08 +0100

    Añado comentario en clase ClientBO

--- src/main/java/org/iesvdm/junit/business/ClientBO.java
+++ src/main/java/org/iesvdm/junit/business/ClientBO.java
@@ -6,6 +6,11 @@ import org.iesvdm.junit.business.exception.DifferentCurrenciesException;
 import org.iesvdm.junit.model.Amount;
 import org.iesvdm.junit.model.Product;
 
+
+/**
+ * 
+ * Esto es un mensaje para probar sincronización con git
+ */
 public interface ClientBO {
 
 	Amount getClientProductsSum(List<Product> products)
